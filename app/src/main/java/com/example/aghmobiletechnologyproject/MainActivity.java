4cdb715aedc7e1039fa6c6d3cf4fb76473474c47
hubert-kompanowski <hubert.kompanowski9@gmail.com>
2020-05-24 21:05:43 +0200

    Adding items to recycleview

--- app/src/main/java/com/example/aghmobiletechnologyproject/MainActivity.java
+++ app/src/main/java/com/example/aghmobiletechnologyproject/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.aghmobiletechnologyproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<MyListData> myListData = new ArrayList<MyListData>();
+    RecyclerView recyclerView = (RecyclerView) findViewById(R.id.tablesList);
+    MyListAdapter adapter = new MyListAdapter(myListData);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button_add_table = findViewById(R.id.buttonAddTable);
+        final EditText nameOfNewTable = findViewById(R.id.nameOfNewTable); //.getText().toString()
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        myListData.add(new MyListData("Table 1"));
+
+        button_add_table.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                myListData.add(new MyListData(nameOfNewTable.getText().toString()));
+
+
+            }
+        });
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/aghmobiletechnologyproject/MyListAdapter.java
@@ -0,0 +1,60 @@
+package com.example.aghmobiletechnologyproject;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+
+public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder> {
+    private List<MyListData> listdata;
+
+    // RecyclerView recyclerView;
+    public MyListAdapter(List<MyListData> listdata) {
+        this.listdata = listdata;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem = layoutInflater.inflate(R.layout.list_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final MyListData myListData = listdata.get(position);
+        holder.textView.setText(listdata.get(position).getDescription());
+        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "click on item: " + myListData.getDescription(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return listdata.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+        public RelativeLayout relativeLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.textView = itemView.findViewById(R.id.textView);
+            relativeLayout = itemView.findViewById(R.id.relativeLayout);
+        }
+    }
+
+}
